c8aa618b3112b3c1f39e5de4679d8777d146b6bf
leandro-lmc <leandro.marqueslmc@gmail.com>
2026-06-30 16:42:36 -0300

    adicionado crud

--- src/index.js
+++ src/index.js
@@ -1,66 +1,139 @@
-// importando o modulo express
-import express from "express";
+import express, { response } from "express";
 import dotenv from "dotenv";
 
-// adicionando as configurações do dotenv
 dotenv.config();
 
-// inicialiazar a aplicação express, construir a aplicação
 const app = express();
+app.use(express.json())
+const porta = process.env.PORTA;
 
-// todo server tem uma porta ou portas
-const port = process.env.PORTA;
+const cursos = [ ];
 
-const produtos = [
-  {
-    matricula: 1,
-    nome: "Coxinha",
-    email: 4.99,
-  },
-];
-
-// Endpoint responsavel por listar todos os produtos
 app.get("/listar", (requisicao, resposta) => {
-  // tratamento de exceção
   try {
-    if (produtos.length === 0) {
-      return resposta
-        .status(200)
-        .json({ mensagem: "Nenhum produto cadastrado!" });
+    if (cursos.length === 0) {
+      return resposta.status(200).json({ mensagem: "Nenhum curso encontrado!" });
     }
-    resposta.status(200).json(produtos);
+    resposta.status(200).json(cursos);
   } catch (error) {
-    resposta.status(500).json({ mensagem: "Erro ao listar os produtos!" });
+    resposta.status(500).json({ mensagem: "Erro ao listar cursos", erro: error });
   }
 });
 
-// Endpoint responsavel por listar um produto por id
-app.get("/listar/:id", (requisicao, resposta) => {
+// Endpoint para listar aluno pelo matricula
+// http://localhost:3000/listar/a92222
+app.get("/listar/:codigo", (requisicao, resposta) => {
   try {
-    const id = parseInt(requisicao.params.id);
-    const produto = produtos.find(produto => produto.id === id);
-    if (!produto) {
-      return resposta.status(404).json({ mensagem: "Produto não encontrado!" });
+    const codigo = requisicao.params.codigo;
+    // const cursos = [{},{},{}]
+    const curso_procurado = cursos.find(curso => curso.codigo === codigo);
+
+    // e se o alunocurso que eu estou procurando não existir?
+    if(!curso_procurado){
+        return resposta.status(200).json({mensagem: "curso não encontrado!"})
     }
-    resposta.status(200).json(produto);
+
+    resposta.status(200).json(curso_procurado)
   } catch (error) {
-    resposta.status(500).json({ mensagem: "Erro ao buscar o produto!" });
+    resposta.status(500).json({mensagem: "Erro ao listar o curso", erro: error})
   }
 });
 
-// Endpoint responsavel por cadastrar produtos
-app.post("/cadastrar", (requisicao, resposta) => {});
+// Endpoint para cadastrar curso 
+app.post("/cadastrar", (requisicao, resposta) => {
+  try {
+    // corpo da requisição com os dados que preciso
+    const { codigo, nome, descrição, cargaHoraria, instrutor, modalidade, quantidadeVagas } = requisicao.body
+    // Vericando se todos os campos foram preenchidos, caso não retorna erro 400
+    if(!codigo || !nome || !descrição || !cargaHoraria || !instrutor || !modalidade || !quantidadeVagas){
+      return resposta.status(400).json({mensagem:"Todos os campos são obrigatorios!"})
+    }
+    // salvando os dados que enviei ao servidor pela req
+    const dados = { codigo, nome, descrição, cargaHoraria, instrutor, modalidade, quantidadeVagas }
+    // Salvando os dados em array(memoria) via push
+    const curso = cursos.push(dados)
 
-// Endpoint responsavel por atualizar todos os dados de um produto
-app.put("/editar", (requisicao, resposta) => {});
+    // resposta informando que o curso foi cadastrado
+    resposta.status(201).json({mensagem: "Cadastro realizado com sucesso!"})
+  } catch (error) {
+    resposta.status(500).json({mensagem: "Erro ao cadastrar curso!", erro:error})
+  }
+})
 
-// Endpoint responsavel por excluir todos os produtos
-app.delete("/excluir", (requisicao, resposta) => {});
+app.put("/editar/:codigo", (requisicao, resposta) => {
+try {
+ const codigo = requisicao.params.matricula
+ const curso = cursos.find(curso => curso.codigo === codigo)
+ if(!codigo){
+  return resposta.status(400).json({mensage:"curso não encontrado!"})
+ }
+ //enviado para o servidor novos dados para editar o curso
+ const {novoNome, novoEmail} = requisicao.body
+ if(!codigo || !nome || !descrição || !cargaHoraria || !instrutor || !modalidade || !quantidadeVagas){
+  return resposta.status(400).json({mensagem:"Todos os campos para edição são obrigatórios!"})
+ }
 
-// Endpoint responsavel por excluir um produto por id
-app.delete("/excluir", (requisicao, resposta) => {});
+ curso.codigo = novocodigo
+ curso.nome = novonome
+ curso.descrição = novodescrição
+ curso.cargaHoraria = novocargaHoraria
+ curso.instrutor = novoinstrutor
+ curso.modalidade = novomodalidade
+  curso.quantidadeVagas = novoquantidadeVagas
 
-// minha aplicação ouvindo a porta 3000
-app.listen(port, () => {
-  console.log(`Servidor executando em http://localhost:${port}`);
+ resposta.status(200).json({mensagem: "curso atualizado com sucesso!"})
+} catch (error){
+  resposta.status(500).json({mensagem:"Erro ao editar o curso!", erro: error})
+}
+});
+
+app.patch("/editar/:codigo", (requisicao, resposta) => {
+  try {
+ const codigo = requisicao.params.matricula
+ const curso = cursos.find(curso => curso.codigo === codigo)
+ if(!curso){
+  return resposta.status(400).json({mensage:"curso não encontrado!"})
+ }
+ const {novocodigo, novonome, novodescrição, novocargaHoraria, novoinstrutor, novomodalidade, novoquantidadeVagas } = requisicao.body
+
+ curso.codigo = novocodigo || curso.codigo
+ curso.nome = novonome || curso.nome
+ curso.descrição = novodescrição || curso.descrição
+ curso.cargaHoraria = novocargaHoraria || curso.cargaHoraria
+ curso.instrutor = novoinstrutor || curso.instrutor
+ curso.modalidade = novomodalidade || curso.modalidade
+ curso.quantidadeVagas = novoquantidadeVagas || curso.quantidadeVagas
+
+  resposta.status(200).json({mensagem: "curso atualizado com sucesso!"})
+
+ } catch (error){
+  resposta.status(500).json({mensagem:"Erro ao editar o aluno!", erro: error})
+}
+});
+
+app.delete("/excluir/todos", (requisicao, resposta) => {
+  try {
+cursos.length = 0
+resposta.status(200).json({mensagem: "Todos os cursos foram excluídos!"})
+  }catch (error){
+    resposta.status(500).json({mensagem: "Erro ao excluir os cursos!", erro: error})
+  }
+});
+
+app.delete("/excluir/:codigo", (requisicao, resposta) => {
+  try {
+     const codigo = requisicao.params.matricula
+    const index = cursos.findIndex(curso => curso.codigo === codigo)
+    if(index === -1){
+      return resposta.status(400).json({mensagem:"Curso não encontrado!"})
+    }
+    curso.splice(index,1)
+    resposta.status(200).json({mensagem: "Curso excluído com sucesso!"})
+  }catch (error) {
+resposta.status(500).json({mensagem: "Erro ao excluir os cursos!", erro: error})
+  }
+})
+
+app.listen(porta, () => {
+  console.log(`O servidor está em execução!`);
 });
